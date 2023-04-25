b254e5c3bbcfdb515f627ad1848604882ad8f950
Luth3rrr <92605914+Luth3rrr@users.noreply.github.com>
2023-04-25 17:19:18 +0300

    Update all.js

--- js/all.js
+++ js/all.js
@@ -1,46 +1,57 @@
-const DEFAULT_URL = "https://my.classoos.com/il/books/textbooks";
+const DEFAULT_URL = "https://classroom.google.com/u/0/";
+const goBtn = document.querySelector(".teacher");
+const changeBtn = document.getElementById("changeBtn");
+let url = localStorage.getItem("savedUrl");
 
-const goBtn = document.querySelector('.teacher');
-const changeBtn = document.getElementById('changeBtn');
-
-let url = localStorage.getItem('savedUrl');
 if (!url) {
-  url = prompt('הכנס קישור לאתר לכפתור מורה מגיעה (תוכל לשנות את זה תמיד והאתר ישמור את בקשתך)', DEFAULT_URL);
+  url = prompt(
+    "הכנס קישור לאתר לכפתור מורה מגיעה) תוכל לשנות את זה תמיד והאתר ישמור את בקשתך(",
+    DEFAULT_URL
+  );
   if (url) {
     if (isValidUrl(url)) {
-      localStorage.setItem('savedUrl', url);
+      localStorage.setItem("savedUrl", url);
     } else {
-      alert('אנא מלא קישור תקין');
+      alert("אנא מלא קישור תקין");
+      url = DEFAULT_URL;
+      localStorage.setItem("savedUrl", url);
+      return;
     }
   } else {
     url = DEFAULT_URL;
-    localStorage.setItem('savedUrl', url);
+    localStorage.setItem("savedUrl", url);
   }
 }
 
-goBtn.addEventListener('click', function() {
-  const url = localStorage.getItem('savedUrl');
+goBtn.addEventListener("click", function () {
+  const url = localStorage.getItem("savedUrl");
   if (url) {
-    const newWindow = window.open(url, '_blank', 'width='+window.innerWidth+',height='+window.innerHeight+',fullscreen=yes,scrollbars=yes');
+    const newWindow = window.open(
+      "https://classroom.google.com/u/0/",
+      "_blank",
+      "width=" +
+        window.innerWidth +
+        ",height=" +
+        window.innerHeight +
+        ",fullscreen=yes,scrollbars=yes"
+    );
     if (newWindow) {
       newWindow.focus();
     }
   }
 });
 
-changeBtn.addEventListener('click', function() {
-  const url = prompt('הכנס קישור לאתר לכפתור מורה מגיעה (תוכל לשנות את זה תמיד והאתר ישמור את בקשתך)', DEFAULT_URL);
+changeBtn.addEventListener("click", function () {
+  const url = prompt(
+    "הכנס קישור לאתר לכפתור מורה מגיעה) תוכל לשנות את זה תמיד והאתר ישמור את בקשתך(",
+    DEFAULT_URL
+  );
   if (url) {
     if (isValidUrl(url)) {
-      localStorage.setItem('savedUrl', url);
+      localStorage.setItem("savedUrl", url);
     } else {
-      alert('אנא מלא קישור תקין');
+      alert("אנא מלא קישור תקין");
       return;
     }
   }
 });
-
-function isValidUrl(url) {
-  // Very basic URL validation
-  return url.match(/^https?:\/\/.+$/);
-}
