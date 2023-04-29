8e690aab5f750da347ba9be17aa3c183a0f0ee8d
Luth3rrr <92605914+Luth3rrr@users.noreply.github.com>
2023-04-29 22:52:37 +0300

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,17 +1,4 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB2q6f49pzNwuRpuuHwbouLXP_sRksH3wg",
-  authDomain: "mugi-test-1718d.firebaseapp.com",
-  databaseURL: "https://mugi-test-1718d-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "mugi-test-1718d",
-  storageBucket: "mugi-test-1718d.appspot.com",
-  messagingSenderId: "533920014849",
-  appId: "1:533920014849:web:a5b9eb7dacc566dc79d600",
-  measurementId: "G-EKNPGEND7B"
-};
 
-firebase.initializeApp(firebaseConfig);
 
 // Check if the user is already signed in
 firebase.auth().onAuthStateChanged(function(user) {
