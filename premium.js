23f6420426fa08817e5e393d06e01336f5796678
Luth3rrr <92605914+Luth3rrr@users.noreply.github.com>
2023-04-29 11:46:46 +0300

    Update premium.js

--- premium.js
+++ premium.js
@@ -13,23 +13,39 @@
   };
 
 firebase.initializeApp(firebaseConfig);
+const loginBtn = document.getElementById('loginBtn');
+const database = firebase.database();
+const accessCodesRef = database.ref('accessCodes');
 
-// Wait for the user to sign in
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    var userId = user.uid;
-    var accessCodeRef = firebase.database().ref('accessCode/' + userId);
+loginBtn.addEventListener('click', function() {
+   const provider = new firebase.auth.GoogleAuthProvider();
+   firebase.auth().signInWithPopup(provider)
+      .then(function(result) {
+         const user = result.user;
+         const uid = user.uid;
+         const displayName = user.displayName;
 
-    accessCodeRef.once('value').then(function(snapshot) {
-      if (snapshot.exists() && snapshot.val() === true) {
-        // User has access to premium features, continue to the site
-      } else {
-        // User does not have access to premium features, redirect to login.html
-        alert('Sorry, you do not have premium access.');
-        window.location.replace('login.html');
-      }
-    });
-  } else {
-    // User is not signed in, do something else
-  }
+         // Check if the user has a valid access code
+         accessCodesRef.child(uid).once('value')
+            .then(function(snapshot) {
+               const accessCodes = snapshot.val();
+
+               if (accessCodes) {
+                  window.location.href = "index.html"; // redirect to main page
+               } else {
+                  // User does not have a valid access code, display error message
+                  alert('Invalid access code');
+                  firebase.auth().signOut(); // Sign the user out
+               }
+            })
+            .catch(function(error) {
+               // Handle errors here
+               console.log(error);
+            });
+
+      })
+      .catch(function(error) {
+         // Handle errors here
+         console.log(error);
+      });
 });
