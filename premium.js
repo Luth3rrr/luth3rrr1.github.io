89ba11680e4df7322aab3463d5612025116487bb
Luth3rrr <92605914+Luth3rrr@users.noreply.github.com>
2023-04-30 14:30:47 +0300

    Update premium.js

--- premium.js
+++ premium.js
@@ -12,8 +12,6 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-
-// Check if the user is already signed in
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     const uid = user.uid;
@@ -28,7 +26,9 @@ firebase.auth().onAuthStateChanged(function(user) {
           // User has a valid access code, do nothing
         } else {
           // User does not have a valid access code, redirect to login page
-          window.location.href = "https://premium.mugitz.com/login.html";
+          if (window.location.pathname !== '/login.html') {
+            window.location.href = "/login.html";
+          }
         }
       })
       .catch(function(error) {
@@ -37,20 +37,8 @@ firebase.auth().onAuthStateChanged(function(user) {
       });
   } else {
     // User is not signed in, redirect to login page
-    window.location.href = "https://premium.mugitz.com/login.html";
+    if (window.location.pathname !== '/login.html') {
+      window.location.href = "/login.html";
+    }
   }
-});
-
-// Login button click event
-const loginBtn = document.getElementById('loginBtn');
-loginBtn.addEventListener('click', function() {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider)
-    .then(function(result) {
-      // User signed in successfully
-    })
-    .catch(function(error) {
-      // Handle errors here
-      console.log(error);
-    });
 });
