c7af3ea047cdc785b4fdd3901af1c2e9ba150f76
Luth3rrr <92605914+Luth3rrr@users.noreply.github.com>
2023-05-11 21:56:32 +0300

    Update all.js

--- js/all.js
+++ js/all.js
@@ -1,7 +1,4 @@
-window.addEventListener("load", function () {
-    const loader = document.querySelector(".loader");
-    loader.className += " hidden"; // class "loader hidden"
-});
+
 const DEFAULT_URL = "https://classroom.google.com/u/0/";
 
 const goBtn = document.querySelector('.teacher');
